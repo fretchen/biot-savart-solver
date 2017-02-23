4a52bfff7c2c40028fb4594ad298b0299861a08f
Fred Jendrzejewski <fnj@kip.uni-heidelberg.de>
2017-02-23 10:58:44 +0100

    Make it possible to create easily a simple wire.

--- BiotSavart/myShapes.py
+++ BiotSavart/myShapes.py
@@ -23,7 +23,7 @@ import cmath
 class Wire:
     '''
     Implements an arbitrary shaped wire
-    '''    
+    '''
     coordz = []
     '''Coordinates of the vertex of the wire in the form [X,Y,Z]'''
     I = complex(1,0)
@@ -43,12 +43,21 @@ class Wire:
         self.Create_Toroidal_Coil(R1, R2, N, step)
         self.Set_Current(1, 0)
         return
-    
+
     def Set_Current(self,modulus,angle):
         '''Sets current with absolute value modulus and phase angle (in radians)'''
         self.I = cmath.rect(modulus,angle)
         return
-    
+
+    def addWire(self, Wire):
+        '''
+        Adds the coordinates of the second class to this one. Both have to have the same current
+        '''
+        X,Y,Z = self.coordz
+        Xo, Yo,Zo = Wire.coordz
+        self.coordz = [r_[X,Xo], r_[Y,Yo],r_[Z,Zo]]
+        return
+
     def Create_Toroidal_Coil(self, R1 , R2 , N , step ):
         '''
         Create_Toroidal_Coil( R1 , R2 , N , step )
@@ -59,17 +68,17 @@ class Wire:
         a=R1
         b=R2
         c=N
-        
+
         t=r_[0:2*pi:step]
-        
+
         X=(a+b*sin(c*t))*cos(t);
         Y=(a+b*sin(c*t))*sin(t);
         Z=b*cos(c*t);
-        
+
         self.coordz = [X,Y,Z]
-        
+
         return
-    
+
     def Create_Solenoid(self, R , N , l , step ):
         '''
         Create_Solenoid(self, R , N , l , step )
@@ -79,25 +88,25 @@ class Wire:
         a = R;
         b = l/(2*pi*N);
         T = l/b;
-        
+
         t=r_[0:T:step]
-        
+
         X = a*cos(t);
         Y = a*sin(t);
         Z = b*t;
-        
+
         self.coordz = [X,Y,Z]
         return
-    
+
     def Create_Loop(self,center,radius,NOP,Orientation='xy'):
         '''
         Create_Loop(self,center,radius,NOP)
         a circle with center defined as
-        a vector CENTER, radius as a scaler RADIS. NOP is 
+        a vector CENTER, radius as a scaler RADIS. NOP is
         the number of points on the circle.
         '''
         t=linspace(0,2*pi,NOP)
-        
+
         if Orientation == 'xy':
             X = center[0]+radius*sin(t)
             Y = center[1]+radius*cos(t)
@@ -110,10 +119,12 @@ class Wire:
             X = center[0]+zeros(NOP)
             Y = center[1]+radius*sin(t)
             Z = center[2]+radius*cos(t)
-        
+
         self.coordz = [X,Y,Z]
         return
-    
+
+
+
     def AugmentWire(self,Theta,Phi,length,Origin=None):
         '''
         AugmentWire(self,Theta,Phi,length,Origin=None)
@@ -121,31 +132,31 @@ class Wire:
         Origin, with inclination Theta and Azimuth Phi. If origin = None then the last
         calculated vertex is used
         '''
-        
+
         #If an origin is not specified, the last vertex is assumed as origin
         if Origin != None:
             newWire = self.__Create_Wire(Origin, Theta, Phi, length)
         else:
             temp = array(self.coordz)
-            newOrigin = temp[:,1]
+            newOrigin = temp[:,-1]
             newWire = self.__Create_Wire(newOrigin, Theta, Phi, length)
-        
+
         #If no coordinates are present, then we simply put the new vertices in the list
         if len(self.coordz)==0:
             self.coordz = newWire
         elif Origin != None:
             X = concatenate((self.coordz[0],newWire[0]),axis = 1)
             Y = concatenate((self.coordz[1],newWire[1]),axis = 1)
-            Z = concatenate((self.coordz[2],newWire[2]),axis = 1) 
+            Z = concatenate((self.coordz[2],newWire[2]),axis = 1)
             self.coordz = [X,Y,Z]
         else:
             X = append(self.coordz[0],newWire[0][1])
             Y = append(self.coordz[1],newWire[1][1])
             Z = append(self.coordz[2],newWire[2][1])
             self.coordz = [X,Y,Z]
-            
+
         return
-    
+
     def __Create_Wire(self,Origin,Theta,Phi,length):
         '''
         Create_Wire(self,Origin,Theta,Phi,length)
@@ -153,68 +164,91 @@ class Wire:
         Origin, with inclination Theta and Azimuth Phi and
         returns its coordinates in the form [X,Y,Z]
         '''
-        
+
         #Computes the unit vector
         ux = cos(Phi)*sin(Theta)
         uy = sin(Phi)*sin(Theta)
         uz = cos(Theta)
-        
+
         u = array([ux,uy,uz])
-        
+
         #Computes the second vertex
         P2 = Origin + length * u
-        
+
         X = array([Origin[0],P2[0]])
         Y = array([Origin[1],P2[1]])
         Z = array([Origin[2],P2[2]])
-                
+
         return [X,Y,Z]
-    
+
+    def Create_Wire(self,Origin,Theta,Phi,length):
+        '''
+        Create_Wire(self,Origin,Theta,Phi,length)
+        creates a single wire lenght long, starting from point
+        Origin, with inclination Theta and Azimuth Phi and
+        returns its coordinates in the form [X,Y,Z]
+        '''
+        #Computes the unit vector
+        ux = cos(Phi)*sin(Theta)
+        uy = sin(Phi)*sin(Theta)
+        uz = cos(Theta)
+
+        u = array([ux,uy,uz])
+
+        #Computes the second vertex
+        P2 = Origin + length * u
+
+        X = array([Origin[0],P2[0]])
+        Y = array([Origin[1],P2[1]])
+        Z = array([Origin[2],P2[2]])
+
+        self.coordz = [X,Y,Z]
+        return
+
     def plotme(self):
         '''Plots itself
-        inactive until ShowPlots is called'''        
+        inactive until ShowPlots is called'''
         import pylab as p
         import mpl_toolkits.mplot3d.axes3d as p3
-        
+
         X = self.coordz[0]
         Y = self.coordz[1]
         Z = self.coordz[2]
-        
+
         fig=p.figure(None)
         ax = p3.Axes3D(fig)
-        
+
         ax.plot(X,Y,Z)
         ax.set_xlabel('X')
         ax.set_ylabel('Y')
         ax.set_zlabel('Z')
-        
+
         self.axis_equal(ax)
-        
+
         p.draw()
-        
+
         return ax
-    
+
     def axis_equal(self,ax):
         '''Makes axis the same size'''
         X = self.coordz[0]
         Y = self.coordz[1]
         Z = self.coordz[2]
-        
+
         Xmax = max(X)
         Ymax = max(Y)
         Zmax = max(Z)
-        
+
         maxx = max(Xmax,Ymax,Zmax)
-        
+
         ax.set_xlim3d(min(X),maxx)
         ax.set_ylim3d(min(Y),maxx)
         ax.set_zlim3d(min(Z),maxx)
         return
-    
+
     def ShowPlots(self):
         '''Triggers pylab.show()'''
         import pylab as p
-        
+
         p.show()
         return
-    
